9d90ac6ec08d5a43070002cdf775535651f1cb36
jayden626 <jayden_626@hotmail.com>
2022-02-28 21:49:47 +1100

    Weather routes set up with express

---
+++ backend/controllers/weatherController.js
@@ -0,0 +1,13 @@
+const asyncHandler = require('express-async-handler')
+
+const getWeather = asyncHandler(async (req, res) => {
+    // if(!req.params.id){
+    //     res.status(400)
+    //     throw new Error('Please add an ID')
+    // }
+    res.status(200).json({ message: `weather got for ${req.params.id}` })
+})
+
+module.exports = {
+    getWeather
+}
